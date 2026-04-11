deea71ada55c4724342020d856fc8d7dae9172b4
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-04-11 09:39:25 +0530

    sm8250-common: Move c2_manifest_vendor.xml to device tree
    Change-Id: I59ceeec26346c0a04a636fa840218dd75b703b7b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -249,7 +249,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
+DEVICE_MANIFEST_FILE += \
+    $(COMMON_PATH)/configs/vintf/manifest.xml \
+    $(COMMON_PATH)/configs/vintf/c2_manifest_vendor.xml
 ifneq ($(TARGET_IS_TABLET),true)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest_phone.xml
 endif
